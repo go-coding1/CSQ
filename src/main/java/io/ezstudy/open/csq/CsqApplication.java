f32634c7f0a63c2f4d120bdcfe83ec62015d32ff
ksj <rhtjrwnszyzy@gmail.com>
2021-11-22 23:33:23 +0900

    first commit

--- src/main/java/io/ezstudy/open/csq/CsqApplication.java
+++ src/main/java/io/ezstudy/open/csq/CsqApplication.java
@@ -1,11 +1,13 @@
 package io.ezstudy.open.csq;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
 @SpringBootApplication
+@EnableAutoConfiguration(exclude = {SecurityAutoConfiguration.class})
 public class CsqApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(CsqApplication.class, args);
     }
